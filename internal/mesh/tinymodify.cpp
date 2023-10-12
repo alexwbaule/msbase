9e0f07dcf68119fc4834a5d179e37a4acf8b17bf
zenggui <anoob@sina.cn>
2023-10-13 00:54:10 +0800

    feature:[]topo

--- internal/mesh/tinymodify.cpp
+++ internal/mesh/tinymodify.cpp
@@ -52,4 +52,50 @@ namespace msbase
 			face[2] = (int)(3 * i + 2);
 		}
 	}
+
+	void removeMeshFaces(trimesh::TriMesh* mesh, const std::vector<int>& removes)
+	{
+		if (!mesh || (mesh->vertices.size() == 0)
+				|| (mesh->faces.size() == 0))
+			return;
+
+		int vcount = mesh->vertices.size();
+		int fcount = mesh->faces.size();
+		std::vector<int> vertexMap(vcount, -1);
+		std::vector<bool> faceStates(fcount, true);
+		for (int i : removes)
+		{
+			if (i >= 0 && i < fcount)
+				faceStates.at(i) = false;
+		}
+
+		std::vector<trimesh::vec3> points;
+		std::vector<trimesh::TriMesh::Face> faces;
+		for (int i = 0; i < fcount; ++i)
+		{
+			if (faceStates.at(i))
+			{
+				const trimesh::TriMesh::Face& f = mesh->faces.at(i);
+				trimesh::TriMesh::Face newFace;
+
+				for(int j = 0; j < 3; ++j)
+				{
+					int index = f[j];
+					if (vertexMap.at(index) >= 0)
+						newFace[j] = vertexMap.at(index);
+					else
+					{
+						points.push_back(mesh->vertices.at(index));
+						int ind = (int)points.size() - 1;
+						vertexMap.at(index) = ind;
+						newFace[j] = ind;
+					}
+				}
+
+				faces.push_back(newFace);
+			}
+		}
+		mesh->vertices.swap(points);
+		mesh->faces.swap(faces);
+	}
 }
--- msbase/mesh/tinymodify.h
+++ msbase/mesh/tinymodify.h
@@ -10,6 +10,8 @@ namespace msbase
 
 	MSBASE_API void reverseTriMesh(trimesh::TriMesh* Mesh);
 	MSBASE_API void fillTriangleSoupFaceIndex(trimesh::TriMesh* mesh);
+
+	MSBASE_API void removeMeshFaces(trimesh::TriMesh* mesh, const std::vector<int>& faces);
 }
 
 #endif // MSBASE_TINYMODIFY_1695183239576_H
