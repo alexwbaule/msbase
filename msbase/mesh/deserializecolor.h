2972d4329893127d7eaf85a124e0b9b4696cbc58
anoob <anoob@sina.cn>
2023-10-21 11:26:12 +0800

    feature:[]add serial

---
+++ msbase/mesh/deserializecolor.h
@@ -0,0 +1,13 @@
+#ifndef MSBASE_DESERIALIZECOLOR_1695441390758_H
+#define MSBASE_DESERIALIZECOLOR_1695441390758_H
+#include "msbase/interface.h"
+#include "trimesh2/TriMesh.h"
+#include "ccglobal/tracer.h"
+
+namespace msbase
+{
+	MSBASE_API trimesh::TriMesh* mergeColorMeshes(trimesh::TriMesh* sourceMesh, const std::vector<std::string>& color2Facets, 
+		std::vector<int>& facet2Facets, ccglobal::Tracer* tracer = nullptr);
+}
+
+#endif // MSBASE_DESERIALIZECOLOR_1695441390758_H
